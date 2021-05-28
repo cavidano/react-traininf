042d9552896f097db76e4f1caba093ab150879cb
Carl Avidano <cavidano@gmail.com>
2021-05-28 15:39:18 -0400

    Converting to functional components

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Button from '../Button';
 
-class User extends Component {
+const User = (props) => {
 
-    render() {
+    // Destructuring?
+    const { id, login, avatar_url, html_url } = props.user;
 
-        // Destructuring?
-        const { id, login, avatar_url, html_url } = this.props.user;
+    return (
 
-        return (
+        <div className="card border">
 
-            <div className="card border">
-
-                <div className="card__head">
-                    <p>{ id }</p>
-                </div>
-
-                <img src={ avatar_url } alt="Placeholder" />
-
-                <div className="card__body">
-                    <h2 className="h4">{ login }</h2>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa obcaecati autem,
-                        ex facilis labore reiciendis.
-                    </p>
-                </div>
-
-                <div className="card__foot border-top font-size-sm">
-                    <ul className="nav nav--horizontal justify-content-between">
-                        <li>
-                            <Button title={id} />
-                        </li>
-                        <li>
-                            <a className="button button--outline border-radius-pill" href={ html_url }>View Profile</a>
-                        </li>
-                    </ul>
-                </div>
-
+            <div className="card__head">
+                <p>{ id }</p>
             </div>
-        
-        );
-    }
+
+            <img src={ avatar_url } alt="Placeholder" />
+
+            <div className="card__body">
+                <h2 className="h4">{ login }</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa obcaecati autem,
+                    ex facilis labore reiciendis.
+                </p>
+            </div>
+
+            <div className="card__foot border-top font-size-sm">
+                <ul className="nav nav--horizontal justify-content-between">
+                    <li>
+                        <Button title={id} />
+                    </li>
+                    <li>
+                        <a className="button button--outline border-radius-pill" href={ html_url }>View Profile</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    
+    );
+    
 }
 
 export default User;
