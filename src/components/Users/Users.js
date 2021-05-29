8ac63ece1b312458354dd87e0d79bade1b72ff97
Carl Avidano <cavidano@gmail.com>
2021-05-29 03:15:34 -0400

    added loading

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-
 import User from './User';
+import Spinner from '../Spinner/Spinner.js';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-    
-    render() {
+const Users = ({ users, loading }) => {
+
+    if (loading) {
+        return <Spinner />
+    } else {
         return (
             <div className="container medium cool margin-y-4">
-                { this.props.users.map(user => (
+                { users.map(user => (
                     <User key={ user.id } user={ user } />
                 )) }
             </div>
         )
     }
+    
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 export default Users;
