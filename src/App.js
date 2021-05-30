ea051b837ce7528ed585b60cb993ed7ea470ef63
Carl Avidano <cavidano@gmail.com>
2021-05-30 04:44:31 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -30,12 +30,22 @@ class App extends Component {
 
   }
 
+  searchUsers = async (text) => {
+    const res = await axios
+      .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({
+      users: res.data.items,
+      loading: false
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Fragment>
           <Header navType="inline" navBP="md" search={true} />
-          <TitleBar title="Github Users" />
+          <TitleBar title="Github Users" searchUsers={this.searchUsers} />
           <Users loading={ this.state.loading } users={this.state.users} />
           <Navbar title="Johnnie" />
         </Fragment> 
--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -11,13 +11,49 @@ class TitleBar extends Component {
         title: PropTypes.string.isRequired
     }
 
+    state = {
+        text: ''
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' })
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
-            <header className="theme-primary padding-y-3">
+            <header className="theme-light padding-y-3">
 
                 <div className="container narrow text-align-center">
-                    <h1>{ this.props.title }</h1>  
+                    <h1>{ this.props.title }</h1>
+
+                    <div className="form-entry font-size-md">
+
+                        <form className="form-entry__field" onSubmit={ this.onSubmit }>
+
+                            <label className="form-entry__field__label screen-reader-only" htmlFor="search-example">
+                                Search
+                            </label>
+                            
+                            <span className="form-entry__field__input">
+                                <input type="text" name="text" value={ this.state.text } onChange={ this.onChange } />
+                                <button className="button button--icon-only" type="submit" aria-label="Home">
+                                    <span className="nyc_icon_search" aria-hidden="true"></span>
+                                </button>
+                            </span>
+
+                        </form>
+
+                    </div>
                 </div>
+
                 
             </header>
         );
