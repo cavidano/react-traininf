a905c31be5d5cb19532c14cfee23aeab64770bd1
Carl Avidano <cavidano@gmail.com>
2021-07-19 09:31:35 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import React, { Fragment } from 'react';
 const About = () => {
     return (
         <Fragment>
-            <h1>About This App</h1>
+        <div className="container narrow margin-y-5">
+            <h1>About the GitHub Finder App</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus autem dicta perferendis voluptate rerum quisquam quasi nostrum quidem repudiandae? Ullam consequatur.
+            </p>
+        </div>
         </Fragment>
     )
 }
